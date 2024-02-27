74fdbacc11db37eb6603abac6aa1acfd3fa70ba5
CarlosAlbertoVillafradesCAMPUS <cavillafrades@gmail.com>
2024-02-27 10:06:09 -0500

    feat: :card_file_box: agregar base de datos y conexion
    se agrego la base de dtaos y se creo la conexion con sequelize
    
    by:carlos villafrades

---
+++ db/conexion.js
@@ -0,0 +1,22 @@
+import { Sequelize } from "sequelize";
+import { envairoments } from "../config/envairoments.js";
+
+const {host, user, password, database, port} = envairoments.my_conexion;
+
+export const sequelize = new Sequelize(user, password, database,{
+    host: host,
+    dialect: "mysql",
+    port: port
+});
+
+export async function validateConexion(req,res,next){
+    try {
+        await sequelize.authenticate()
+        next()
+    } catch (error) {
+        res.status(400).json({status:400, message:"Error al conectar con la base de datos"})
+    }
+}
+
+
+
